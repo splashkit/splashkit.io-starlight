54521556e07179ed40f2ff320e1954debbcecec5
omckeon <ordmckeon@gmail.com>
2024-05-10 20:32:02 +0000

    Add usage-examples folders with README files and usage-examples CONTRIBUTING file

---
+++ src/assets/usage-examples-code/terminal/write_line/write_line-1-hello-world.cs
@@ -0,0 +1,17 @@
+﻿/**
+ * Usage Example: Printing a string to the terminal.
+ **/
+
+using static SplashKitSDK.SplashKit;
+
+// Example 1: Print explicit string
+WriteLine("Hello World");
+
+// Example 2: Print value of string variable
+string message = "Hello World from 'message' variable";
+WriteLine(message);
+
+// Example 3: Print combination of explicit string and value of string variable
+string hello = "Hello";
+WriteLine(hello + " World!\nDon't forget spaces between words when printing to the terminal!");
+WriteLine("Otherwise you get this: " + hello + "World!");
