370b5b127b8f0e4812c4eab3fd4ef2564f2a704a
sharvani28 <sharvanikandala@gmail.com>
2024-08-09 01:25:23 +0000

    Addressed peer review comments: Updated fill_triangle files

--- src/assets/usage-examples-code/graphics/fill_triangle/fill_triangle.cpp
+++ src/assets/usage-examples-code/graphics/fill_triangle/fill_triangle.cpp
@@ -4,11 +4,11 @@ int main()
 {
     open_window("Fill triangle Example", 800, 600);
     clear_screen();
+
     fill_triangle(COLOR_RED, 100, 100, 200, 200, 300, 100);
     refresh_screen();
 
     delay(5000);
-
     close_all_windows();
 
     return 0;
--- src/assets/usage-examples-code/graphics/fill_triangle/fill_triangle.cs
+++ src/assets/usage-examples-code/graphics/fill_triangle/fill_triangle.cs
@@ -2,11 +2,10 @@ using SplashKitSDK;
 using static SplashKitSDK.SplashKit;
 
 OpenWindow("Fill Triangle Example", 800, 600);
-
 ClearScreen();
+
 FillTriangle(ColorRed(), 100, 100, 200, 200, 300, 100);
 RefreshScreen();
 
 Delay(5000);
-
 CloseAllWindows();
