8c6c4b7160ea6fae0beec66f6596b4aaff5ba3cf
Jinx-means-jinx <www.2927196163@qq.com>
2024-08-18 01:07:28 +1000

    Added new usage example (Background Color) and the code and image used in Background Color

---
+++ src/assets/usage-examples-code/graphics/clear_screen/clear_screen-background-color.cpp
@@ -0,0 +1,13 @@
+#include "splashkit.h"
+
+int main()
+{
+    open_window("Back Ground", 800, 600);
+    clear_screen(COLOR_BLUE);
+    refresh_screen(60);
+    delay(5000);
+
+    close_all_windows();
+
+    return 0;
+}
