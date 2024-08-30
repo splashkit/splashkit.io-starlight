b48dfed87ea306150ffe1afb82ae1c8dd48f36a8
sharvani28 <139297620+sharvani28@users.noreply.github.com>
2024-08-30 18:52:40 +1000

    Update fill_triangle_on_bitmap-1-basic-bitmap.cpp
    Space adjustments

--- src/assets/usage-examples-code/graphics/fill_triangle_on_bitmap/fill_triangle_on_bitmap-1-basic-bitmap.cpp
+++ src/assets/usage-examples-code/graphics/fill_triangle_on_bitmap/fill_triangle_on_bitmap-1-basic-bitmap.cpp
@@ -3,17 +3,17 @@
 int main()
 {
     open_window("Underwater Adventure", 1000, 500);
-    clear_screen();
     
+    clear_screen();
     bitmap underwater_scene = load_bitmap("underwater_scene", "underwater_scene.png");
     draw_bitmap(underwater_scene, 0, 0);
     draw_text("Colourful Fish", COLOR_RED, "arial", 123, 240, 200);
     draw_line(COLOR_RED, 280, 220, 305, 255);
     fill_triangle(COLOR_RED, 285, 250, 310, 240, 310, 260);
-    
     refresh_screen();
     delay(5000);
     
     close_all_windows();
+    
     return 0;
 }
