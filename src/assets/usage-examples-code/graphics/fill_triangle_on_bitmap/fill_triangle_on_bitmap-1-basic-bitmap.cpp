42c73fb749c1996ed3ece99dd3e3541767523c97
sharvani28 <sharvanikandala@gmail.com>
2024-09-08 23:04:59 +1000

    Updated code to include function

--- src/assets/usage-examples-code/graphics/fill_triangle_on_bitmap/fill_triangle_on_bitmap-1-basic-bitmap.cpp
+++ src/assets/usage-examples-code/graphics/fill_triangle_on_bitmap/fill_triangle_on_bitmap-1-basic-bitmap.cpp
@@ -2,18 +2,32 @@
 
 int main()
 {
+    // Open a window
     open_window("Underwater Adventure", 1000, 500);
-    
-    clear_screen();
+
+    // Load the bitmap
     bitmap underwater_scene = load_bitmap("underwater_scene", "underwater_scene.png");
+
+    // Fill the triangle on the bitmap
+    double x1 = 285, y1 = 250;
+    double x2 = 310, y2 = 240;
+    double x3 = 310, y3 = 260;
+    color triangle_color = COLOR_RED;
+
+    fill_triangle_on_bitmap(underwater_scene, triangle_color, x1, y1, x2, y2, x3, y3);
+
+    // Draw the bitmap and other graphics
     draw_bitmap(underwater_scene, 0, 0);
     draw_text("Colourful Fish", COLOR_RED, "arial", 123, 240, 200);
     draw_line(COLOR_RED, 280, 220, 305, 255);
-    fill_triangle(COLOR_RED, 285, 250, 310, 240, 310, 260);
+
+    // Refresh the screen and wait
     refresh_screen();
     delay(5000);
-    
+
+    // Close all windows
     close_all_windows();
-    
+
     return 0;
 }
+ 
--- src/assets/usage-examples-code/graphics/fill_triangle_on_bitmap/fill_triangle_on_bitmap-1-basic-bitmap.cs
+++ src/assets/usage-examples-code/graphics/fill_triangle_on_bitmap/fill_triangle_on_bitmap-1-basic-bitmap.cs
@@ -3,13 +3,24 @@ using static SplashKitSDK.SplashKit;
 
 OpenWindow("Underwater Adventure", 1000, 500);
 
-ClearScreen();
-Bitmap underwaterScene = new Bitmap("underwater_scene", "underwater_scene.png");
+// Load the bitmap
+Bitmap underwaterScene = LoadBitmap("underwater_scene", "underwater_scene.png");
+
+// Fill the triangle on the bitmap
+double x1 = 285, y1 = 250;
+double x2 = 310, y2 = 240;
+double x3 = 310, y3 = 260;
+
+FillTriangleOnBitmap(underwaterScene, ColorRed(), x1, y1, x2, y2, x3, y3);
+
+// Draw the bitmap and other graphics
 DrawBitmap(underwaterScene, 0, 0);
-DrawText("Colourful Fish", Color.Red, "arial", 123, 240, 200);
-DrawLine(Color.Red, 280, 220, 305, 255);
-FillTriangle(Color.Red, 285, 250, 310, 240, 310, 260);
+DrawText("Colourful Fish", ColorRed(), "arial", 123, 240, 200);
+DrawLine(ColorRed(), 280, 220, 305, 255);
+
+// Refresh the screen and wait
 RefreshScreen();
 Delay(5000);
 
+// Close all windows
 CloseAllWindows();
