98258c4013f449f48025d20a7f90a6d1d04bf3fb
Jinx-means-jinx <www.2927196163@qq.com>
2024-09-12 19:13:54 +1000

    small change

--- src/assets/usage-examples-code/graphics/fill_ellipse/fill_ellipse-1-ellipse-color.cpp
+++ src/assets/usage-examples-code/graphics/fill_ellipse/fill_ellipse-1-ellipse-color.cpp
@@ -6,6 +6,8 @@ int main()
     clear_screen();
     fill_ellipse(COLOR_BLUE, 200, 200, 400, 200);
     refresh_screen(60);
+    draw_rectangle(COLOR_RED, 200, 200, 400, 200);
+    refresh_screen(60);
     delay(4000);
 
     close_all_windows();
--- src/assets/usage-examples-code/graphics/fill_ellipse/fill_ellipse-1-ellipse-color.cs
+++ src/assets/usage-examples-code/graphics/fill_ellipse/fill_ellipse-1-ellipse-color.cs
@@ -5,6 +5,8 @@ ClearScreen();
 
 FillEllipse(ColorBlue(), 200, 200, 400, 200);
 RefreshScreen(60);
+DrawRectangle(ColorRed(), 200, 200, 400, 200);
+RefreshScreen(60);
 Delay(4000);
 
 CloseAllWindows();
