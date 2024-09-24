d8c31d651a7a217ae3f082d8644fe0646324e5f8
omckeon <ordmckeon@gmail.com>
2024-09-24 03:28:31 +0000

    Minor code formatting update

--- src/assets/usage-examples-code/graphics/fill_ellipse/fill_ellipse-1-ellipse-color.cpp
+++ src/assets/usage-examples-code/graphics/fill_ellipse/fill_ellipse-1-ellipse-color.cpp
@@ -3,11 +3,13 @@
 int main()
 {
     open_window("Fill Ellipse", 800, 600);
+
     clear_screen();
     fill_ellipse(COLOR_BLUE, 200, 200, 400, 200);
-    refresh_screen(60);
+    // Added rectangle with same arguments as above for x, y, width and height
     draw_rectangle(COLOR_RED, 200, 200, 400, 200);
-    refresh_screen(60);
+    refresh_screen();
+    
     delay(4000);
 
     close_all_windows();
