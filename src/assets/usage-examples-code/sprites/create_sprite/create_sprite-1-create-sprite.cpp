ede8b800e5abe9509c3796a0c1c16e3aa78707a1
omckeon <ordmckeon@gmail.com>
2024-10-09 04:03:59 +0000

    small code formatting updates

--- src/assets/usage-examples-code/sprites/create_sprite/create_sprite-1-create-sprite.cpp
+++ src/assets/usage-examples-code/sprites/create_sprite/create_sprite-1-create-sprite.cpp
@@ -10,15 +10,11 @@ int main()
     // Create the player sprite
     sprite player_sprite = create_sprite(player);
 
-    // Set the sprite coordinates in reference to the window
     sprite_set_x(player_sprite, 300);
     sprite_set_y(player_sprite, 300);
 
     clear_screen(COLOR_BLACK);
-
-    // Draw the sprite
     draw_sprite(player_sprite);
-    
     refresh_screen();
     delay(5000);
     close_all_windows();
--- src/assets/usage-examples-code/sprites/create_sprite/create_sprite-1-create-sprite.cs
+++ src/assets/usage-examples-code/sprites/create_sprite/create_sprite-1-create-sprite.cs
@@ -9,15 +9,11 @@ Bitmap player = LoadBitmap("player_bitmap", "player.png");
 // Create the player sprite
 Sprite playerSprite = CreateSprite(player);
 
-// Set the sprite coordinates in reference to the window
 playerSprite.X = 300;
 playerSprite.Y = 300;
 
 ClearScreen(ColorBlack());
-
-// Draw the sprite
 DrawSprite(playerSprite);
-
 RefreshScreen();
 Delay(5000);
 CloseAllWindows();
