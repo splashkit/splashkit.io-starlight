64ec945ffebf30ed71c8dd3aec095dcd2f3a2e5d
Rachel Mei Chu <93307743+rmachu@users.noreply.github.com>
2024-12-02 22:29:05 +1100

    Language update to Clear screen-background colour usage example (#244)

---
+++ public/usage-examples/graphics/clear_screen/clear_screen-1-simple-oop.cs
@@ -0,0 +1,19 @@
+﻿using SplashKitSDK;
+
+namespace ClearScreenExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            SplashKit.OpenWindow("Blue Background", 800, 600);
+
+            //Use Clear Screen to change the background color to blue
+            SplashKit.ClearScreen(Color.Blue);
+
+            SplashKit.RefreshScreen(60);
+            SplashKit.Delay(4000);
+            SplashKit.CloseAllWindows();
+        }
+    }
+}
