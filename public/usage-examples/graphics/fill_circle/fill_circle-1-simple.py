12c630f038f67443789eba7bbfb6a685e3010d10
EthanRin <141208363+EthanRin@users.noreply.github.com>
2024-12-02 23:07:27 +1100

    Usage Update For Fill Circle (#243)

---
+++ public/usage-examples/graphics/fill_circle/fill_circle-1-simple.py
@@ -0,0 +1,11 @@
+from splashkit import *
+
+open_window("Fill Circle", 800, 600)
+
+clear_screen(color_white())
+
+fill_circle(color_blue(), 300, 300, 200) 
+refresh_screen()
+delay(4000)
+
+close_all_windows()
