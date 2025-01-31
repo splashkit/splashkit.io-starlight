224edfcf40538e9759626f75fff6a6bc8184d6f0
SimonRhook <127938862+SimonRhook@users.noreply.github.com>
2025-01-31 18:39:28 +1100

    Added usage example for random_window_point (#264)

---
+++ public/usage-examples/geometry/random_window_point-1-example.cpp
@@ -0,0 +1,29 @@
+#include "splashkit.h"
+
+int main()
+{
+    // Create Window
+    window window = open_window("Portal", 600, 600);
+
+    // Load portal sprites
+    load_bitmap("blue_portal", "bluePortal.png");
+    load_bitmap("orange_portal", "orangePortal.png");
+    sprite blue_portal = create_sprite(bitmap_named("blue_portal"));
+    sprite orange_portal = create_sprite(bitmap_named("orange_portal"));
+
+    // Set random portal location
+    sprite_set_position(blue_portal, random_window_point(window));
+    sprite_set_position(orange_portal, random_window_point(window));
+
+    clear_window(window, color_black());
+
+    // Draw the sprite
+    draw_sprite(blue_portal);
+    draw_sprite(orange_portal);
+
+    refresh_screen();
+    delay(5000);
+    close_window(window);
+
+    return 0;
+}
