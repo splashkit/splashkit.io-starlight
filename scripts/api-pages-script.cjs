b720db4cf320142c21e2cec2e74fdefa9e1d68e6
omckeon <ordmckeon@gmail.com>
2025-02-13 11:04:08 +0000

    Fix typo

--- scripts/api-pages-script.cjs
+++ scripts/api-pages-script.cjs
@@ -724,7 +724,7 @@ for (const categoryKey in jsonData) {
           mdxContent += "**Usage:**\n\n"
           usageHeading = true;
         }
-        mdxContent += `<Accordion title="See Implemenations in Guides" uniqueID={${JSON.stringify(func.unique_global_name + "_guides")}} customButton="guidesAccordion">\n\n`
+        mdxContent += `<Accordion title="See Implementations in Guides" uniqueID={${JSON.stringify(func.unique_global_name + "_guides")}} customButton="guidesAccordion">\n\n`
 
         mdxContent += `<ul>`
         allGuides.forEach((guide) => {
