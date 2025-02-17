a7fab67092e6c201e6d1d3aba1192b4eccc5d408
SimonRhook <127938862+SimonRhook@users.noreply.github.com>
2025-02-17 20:22:17 +1100

    Added usage example for point_in_rectangle (#273)

---
+++ public/usage-examples/geometry/point_in_rectangle-1-example.py
@@ -0,0 +1,47 @@
+from splashkit import *
+
+# Variable Declarations
+mouse_pt = Point2D
+boundary = rectangle_from(150, 150, 300, 100)
+text = str
+color1 = Color
+color2 = Color
+flashing_timer = create_timer("flash time")
+
+open_window("Cursor Jail", 600, 600)
+start_timer(flashing_timer)
+
+while not quit_requested():
+    process_events()
+
+    # Get mouse position
+    mouse_pt = mouse_position()
+
+    # Check if mouse is in the boundary
+    if point_in_rectangle(mouse_pt, boundary):
+        text = ""
+        color1 = color_green()
+        color2 = color_green()
+        reset_timer(flashing_timer)
+    else:
+        # Flash screen red and blue if mouse has escaped boundary
+        text = "JAILBREAK"
+        color1 = color_royal_blue()
+        color2 = color_dark_red()
+
+    # Draw UI using timer ticks
+    if timer_ticks(flashing_timer) < 500:
+        clear_screen(color1)
+        fill_rectangle_record(color_white(), boundary)
+        refresh_screen()
+
+    if timer_ticks(flashing_timer) >= 500 and timer_ticks(flashing_timer) < 1000:
+        clear_screen(color2)
+        fill_rectangle_record(color_white(), boundary)
+        draw_text_no_font_no_size(text, color_black(), 250, 400)
+        refresh_screen()
+
+    if timer_ticks(flashing_timer) >= 1000:
+        reset_timer(flashing_timer)
+
+close_all_windows()
