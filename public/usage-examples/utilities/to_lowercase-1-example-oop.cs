ca435d896e647614e337feeba4204c47cf9b67f1
Breezy <114720008+breezy-codes@users.noreply.github.com>
2025-02-17 18:33:35 +1100

    Usage Examples for To Upper Case and for To Lower Case (#259)

---
+++ public/usage-examples/utilities/to_lowercase-1-example-oop.cs
@@ -0,0 +1,18 @@
+﻿using SplashKitSDK;
+
+namespace ToLowercaseExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            SplashKit.WriteLine("Type a phrase in ALL CAPS (SHOUT IT!):");
+            string input = SplashKit.ReadLine();
+
+            // Convert input to lowercase
+            string quieted = SplashKit.ToLowercase(input);
+
+            SplashKit.WriteLine("Calm down... here it is in lowercase: " + quieted);
+        }
+    }
+}
---
+++ public/usage-examples/utilities/to_lowercase-1-example-top-level.cs
@@ -0,0 +1,9 @@
+﻿using static SplashKitSDK.SplashKit;
+
+WriteLine("Type a phrase in ALL CAPS (SHOUT IT!):");
+string input = ReadLine();
+
+// Convert input to lowercase
+string quieted = ToLowercase(input);
+
+WriteLine("Calm down... here it is in lowercase: " + quieted);
