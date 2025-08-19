5b22b289b68f0a94ecd5f5f15c86def5fa169e87
CATHoDE33 <94217306+CATHoDE33@users.noreply.github.com>
2025-08-20 00:14:28 +1000

    Add files via upload

---
+++ public/usage-examples/graphics/bitmap_bounding_circle-1-example-oop.cs
@@ -0,0 +1,35 @@
+﻿using SplashKitSDK;
+
+namespace BitmapBoundingCircleExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            SplashKit.OpenWindow("Bitmap Bounding Circle", 800, 600);
+
+            Bitmap vertical_bitmap = SplashKit.LoadBitmap("vertical_bitmap", "image1.jpeg");
+            Bitmap horizontal_bitmap = SplashKit.LoadBitmap("horizontal_bitmap", "image2.png");
+
+            // The 'bitmap_bounding_circle' function creates a circle with a circumference that encompasses the four vertices of it's associated bitmap
+            // A Point2D is also given to determine the circle's position
+            Circle vert_bitmap_circle = SplashKit.BitmapBoundingCircle(vertical_bitmap, SplashKit.PointAt(210, 210));
+            Circle hori_bitmap_circle = SplashKit.BitmapBoundingCircle(horizontal_bitmap, SplashKit.PointAt(580, 400));
+
+            while (!SplashKit.QuitRequested())
+            {
+                SplashKit.ProcessEvents();
+                SplashKit.ClearScreen(Color.White);
+
+                SplashKit.DrawBitmap(vertical_bitmap, 141, 60);
+                SplashKit.DrawBitmap(horizontal_bitmap, 480, 344);
+
+                SplashKit.DrawCircle(Color.Black, vert_bitmap_circle);
+                SplashKit.DrawCircle(Color.Black, hori_bitmap_circle);
+
+                SplashKit.RefreshScreen();
+            }
+            SplashKit.CloseAllWindows();
+        }
+    }
+}
---
+++ public/usage-examples/graphics/bitmap_bounding_circle-1-example-top-level.cs
@@ -0,0 +1,27 @@
+﻿using SplashKitSDK;
+using static SplashKitSDK.SplashKit;
+
+OpenWindow("Bitmap Bounding Circle", 800, 600);
+
+Bitmap vertical_bitmap = LoadBitmap("vertical_bitmap", "image1.jpeg");
+Bitmap horizontal_bitmap = LoadBitmap("horizontal_bitmap", "image2.png");
+
+// The 'bitmap_bounding_circle' function creates a circle with a circumference that encompasses the four vertices of it's associated bitmap
+// A Point2D is also given to determine the circle's position
+Circle vert_bitmap_circle = BitmapBoundingCircle(vertical_bitmap, PointAt(210, 210));
+Circle hori_bitmap_circle = BitmapBoundingCircle(horizontal_bitmap, PointAt(580, 400));
+
+while (!QuitRequested())
+{
+    ProcessEvents();
+    ClearScreen(ColorWhite());
+
+    DrawBitmap(vertical_bitmap, 141, 60);
+    DrawBitmap(horizontal_bitmap, 480, 344);
+
+    DrawCircle(ColorBlack(), vert_bitmap_circle);
+    DrawCircle(ColorBlack(), hori_bitmap_circle);
+
+    RefreshScreen();
+}
+CloseAllWindows();
